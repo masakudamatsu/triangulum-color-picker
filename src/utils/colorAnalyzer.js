0555e1303923bb6f77006feafdb26b3cdc866a30
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-12-10 16:43:01 +0900

    Add colorAnalyzer
    Convert css color code into its luminance and its shares of pure hue and neutral color

---
+++ src/utils/colorAnalyzer.js
@@ -0,0 +1,63 @@
+import parseColor from 'parse-color';
+import getContrastRatio from './getContrastRatio';
+
+const colorAnalyzer = colorCode => {
+  // https://github.com/substack/parse-color
+  const {rgb, hsl} = parseColor(colorCode);
+  const [red, green, blue] = rgb;
+
+  const luminance = getContrastRatio(red, green, blue);
+
+  let saturation, hue, neutralColor;
+
+  // If the input is neutral color
+  if (red === green && red === blue) {
+    saturation = 0;
+    hue = null;
+    neutralColor = red;
+
+    // If the input is pure hue
+  } else if (red === 0 || green === 0 || blue === 0) {
+    saturation = 100;
+    hue = {
+      hsl: `hsl(${hsl[0]}, ${hsl[1]}, ${hsl[2]})`,
+      rgb: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`,
+    };
+    neutralColor = null;
+
+    // If the input is desaturated color
+  } else {
+    const inputMax = Math.max(...rgb);
+    const inputMin = Math.min(...rgb);
+    const inputMid =
+      (red === inputMax && green === inputMin) ||
+      (red === inputMin && green === inputMax)
+        ? blue
+        : (red === inputMax && blue === inputMin) ||
+          (red === inputMin && blue === inputMax)
+        ? green
+        : red;
+
+    const pureHueShare = (inputMax - inputMin) / 255;
+    saturation = Math.round(pureHueShare * 10000) / 100; // Keep two decimal places
+
+    const grayValue = inputMin / (1 - pureHueShare);
+    neutralColor = Math.round(grayValue);
+
+    const pureHueHSL = `hsl(${hsl[0]}, 100, 50)`;
+    const pureHueRgbArray = parseColor(pureHueHSL).rgb;
+    hue = {
+      hsl: pureHueHSL,
+      rgb: `rgb(${pureHueRgbArray[0]}, ${pureHueRgbArray[1]}, ${pureHueRgbArray[2]})`,
+    };
+  }
+
+  return {
+    luminance: luminance,
+    saturation: saturation,
+    hue: hue,
+    neutralColor: neutralColor,
+  };
+};
+
+export default colorAnalyzer;
