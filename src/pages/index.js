a726894e96baac5fabd0076354da38ddf769164e
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-12-11 17:43:11 +0900

    Pass the pure hue as a derived state to Canvas component
    Allow the color triangle to change by the user-selected color

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import Canvas from 'src/components/Canvas';
 import Template from 'src/components/Template';
 import styled from 'styled-components';
 
+import colorAnalyzer from 'src/utils/colorAnalyzer';
+import parseColor from 'parse-color';
+
 const FlexContainer = styled.div`
   align-items: center;
   display: flex;
@@ -12,10 +15,18 @@ const FlexContainer = styled.div`
 `;
 
 function HomePage() {
+  const userColor = 'rgb(29, 161, 242)';
+  const {luminance, saturation, hue, neutralColor} = colorAnalyzer(userColor);
+  const pureHue = {
+    r: parseColor(hue.rgb).rgb[0],
+    g: parseColor(hue.rgb).rgb[1],
+    b: parseColor(hue.rgb).rgb[2],
+  };
+
   return (
     <FlexContainer>
       <Template />
-      <Canvas />
+      <Canvas pureHue={pureHue} />
     </FlexContainer>
   );
 }
