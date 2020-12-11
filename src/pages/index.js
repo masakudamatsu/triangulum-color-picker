0cb5502ca7d6ff015ef3f837b52f5de8417756af
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-12-11 16:50:05 +0900

    Center-align HTML elements on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 import Template from 'src/components/Template';
+import styled from 'styled-components';
+
+const FlexContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  width: 100%;
+`;
 
 function HomePage() {
-  return <Template />;
+  return (
+    <FlexContainer>
+      <Template />
+    </FlexContainer>
+  );
 }
 
 export default HomePage;
