d63d17c20e85a868de4e7237eece2dd5cf2a0d4e
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-01-25 21:47:15 +0900

    Add image snapshot tests for responsive layouts

--- cypress/e2e/snapshots.js
+++ cypress/e2e/snapshots.js
@@ -1,13 +1,35 @@
+import {threeColumns, twoColumns} from 'src/utils/breakpoints';
+
 const twitterBlue = 'rgb(29, 161, 242)';
 const mcdonaldsRed = 'rgb(191, 12, 12)';
 const grey = 'rgb(129, 129, 129)';
 const white = 'rgb(255, 255, 255)';
 
-it.only('Entire UI is correctly shown', () => {
-  cy.viewport('iphone-5'); // 320 x 568 (see https://docs.cypress.io/api/commands/viewport.html#Arguments)
-  cy.visit('/');
-  cy.findByLabelText(/color code/i).type(twitterBlue);
-  cy.matchImageSnapshot('entire-ui', {capture: 'fullPage'});
+describe.only('Entire UI is correctly shown', () => {
+  it('for narrowest screen size', () => {
+    cy.viewport('iphone-5'); // 320 x 568 (see https://docs.cypress.io/api/commands/viewport.html#Arguments)
+    cy.visit('/');
+    cy.findByLabelText(/color code/i).type(twitterBlue);
+    cy.matchImageSnapshot('entire-ui-narrowest', {capture: 'fullPage'});
+  });
+  it('for tablet screen size', () => {
+    cy.viewport('ipad-2'); // 768 x 1024 (see https://docs.cypress.io/api/commands/viewport.html#Arguments)
+    cy.visit('/');
+    cy.findByLabelText(/color code/i).type(twitterBlue);
+    cy.matchImageSnapshot('entire-ui-tablet', {capture: 'fullPage'});
+  });
+  it('for laptop screen size', () => {
+    cy.viewport(twoColumns.minWidth, twoColumns.minHeight);
+    cy.visit('/');
+    cy.findByLabelText(/color code/i).type(twitterBlue);
+    cy.matchImageSnapshot('entire-ui-laptop', {capture: 'fullPage'});
+  });
+  it('for desktop screen size', () => {
+    cy.viewport(threeColumns.minWidth, threeColumns.minHeight);
+    cy.visit('/');
+    cy.findByLabelText(/color code/i).type(twitterBlue);
+    cy.matchImageSnapshot('entire-ui-desktop', {capture: 'fullPage'});
+  });
 });
 
 describe('Entering css color code shows the color triangle diagram', () => {
