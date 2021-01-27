ca701c26e1b0f9400181666da4120f0c6b3dca65
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-01-27 19:43:00 +0900

    Move the head element's content to _app.js #78
    Avoid the known bug from placing the viewport meta tag in _document.js https://github.com/vercel/next.js/blob/master/errors/no-document-viewport-meta.md

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,78 @@
+import Head from 'next/head';
+
 import GlobalStyle from 'src/utils/GlobalStyle';
+
+const structuredData = {
+  '@context': 'http://schema.org',
+  '@type': 'WebApplication',
+  name: 'Triangulum Color Picker',
+  url: 'triangulum-color-picker.app',
+  applicationCategory: 'DesignApplication',
+  genre: 'color',
+  browserRequirements: 'Requires JavaScript. Requires HTML5.',
+  softwareVersion: '1.0.0-beta.1',
+  operatingSystem: 'All',
+  offers: {
+    '@type': 'Offer',
+    price: '0',
+  },
+};
 
 export default function App({Component, pageProps}) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Triangulum Color Picker</title>
+        <meta
+          name="description"
+          content="A front-end web app to pick a tint/tone/shade of hues more logically and more intuitively than the standard color pickers"
+        />
+        {/* SVG-favicon-compatible browsers */}
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+
+        {/*  iOS Home Screen */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Triangulum" />
+
+        {/*  Old browsers that do not recognize SVG as favicon */}
+        <link
+          rel="alternate icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="alternate icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        {/*  Android Chrome */}
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="application-name" content="Triangulum" />
+
+        {/*  MacOS Safari pinned tab / touch bar */}
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4a4a4a" />
+
+        {/* Windows Start Menu Tile */}
+        <meta name="msapplication-TileColor" content="#4a4a4a" />
+
+        {/* Fallback? */}
+        <meta name="theme-color" content="#4a4a4a" />
+
+        {/* Structured data: see https://developers.google.com/search/docs/data-types/software-app#softwareapplication */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{__html: JSON.stringify(structuredData)}}
+          key="structured-data"
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />{' '}
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,22 +3,6 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
-const structuredData = {
-  '@context': 'http://schema.org',
-  '@type': 'WebApplication',
-  name: 'Triangulum Color Picker',
-  url: 'triangulum-color-picker.app',
-  applicationCategory: 'DesignApplication',
-  genre: 'color',
-  browserRequirements: 'Requires JavaScript. Requires HTML5.',
-  softwareVersion: '1.0.0-beta.1',
-  operatingSystem: 'All',
-  offers: {
-    '@type': 'Offer',
-    price: '0',
-  },
-};
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -48,59 +32,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Triangulum Color Picker</title>
-          <meta
-            name="description"
-            content="A front-end web app to pick a tint/tone/shade of hues more logically and more intuitively than the standard color pickers"
-          />
-          {/* SVG-favicon-compatible browsers */}
-          <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-
-          {/*  iOS Home Screen */}
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <meta name="apple-mobile-web-app-title" content="Triangulum" />
-
-          {/*  Old browsers that do not recognize SVG as favicon */}
-          <link
-            rel="alternate icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="alternate icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-
-          {/*  Android Chrome */}
-          <link rel="manifest" href="/site.webmanifest" />
-          <meta name="application-name" content="Triangulum" />
-
-          {/*  MacOS Safari pinned tab / touch bar */}
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4a4a4a" />
-
-          {/* Windows Start Menu Tile */}
-          <meta name="msapplication-TileColor" content="#4a4a4a" />
-
-          {/* Fallback? */}
-          <meta name="theme-color" content="#4a4a4a" />
-
-          {/* Structured data: see https://developers.google.com/search/docs/data-types/software-app#softwareapplication */}
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{__html: JSON.stringify(structuredData)}}
-            key="structured-data"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
