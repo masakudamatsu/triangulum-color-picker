90020c337a924ae5d72ce802f119af0fe6bf1b5d
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-01-29 12:18:03 +0900

    Remove the redundant comment #19
    We no longer use web fonts. The use of _document.js is only for Styled Components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-// Based on (1) https://nextjs.org/docs/advanced-features/custom-document (15 Sep, 2020) (for using Google Fonts) and (2) https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_document.js (1 Sep, 2020) (for using styled-components)
+// Based on https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_document.js (1 Sep, 2020) (for using styled-components)
 
 import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
