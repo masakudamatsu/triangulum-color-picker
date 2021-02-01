4f96dda6c1305923d35ec22692022d021338b220
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-01 14:12:46 +0900

    Replace the preload with its fallback print media toggle technique
    Test if this approach works

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -28,14 +28,10 @@ export default function App({Component, pageProps}) {
         {/* Use Google Fonts server for Poppins */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap"
-        />
-
-        <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap"
+          media="print"
+          onload="this.media='all'"
         />
 
         <meta
