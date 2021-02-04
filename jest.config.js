894cb59f0eafb571c33b0eb3f651a0240256006f
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-04 09:54:55 +0900

    Set up Jest Code Coverage Report

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,32 @@ const path = require('path');
 
 module.exports = {
   clearMocks: true, // run jest.clearAllMocks() after each test
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/pages/_*.js',
+    '!**/*.test.js',
+    '!**/*.spec.js',
+  ], // https://jestjs.io/docs/en/configuration#collectcoveragefrom-array
+  coverageThreshold: {
+    global: {
+      statements: 50,
+      branches: 46,
+      functions: 51,
+      lines: 51,
+    },
+    './src/utils/colorAnalyzer.js': {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+    './src/utils/getContrastRatio.js': {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
   moduleDirectories: [
     'node_modules',
     __dirname,
