dcfc274add25884ce29ed71916063e023f8f5f70
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-13 21:53:38 +0900

    feat: add icon link to help documents on chroma and luminance
    help the user understand what we mean by chroma and luminance
    
    close #156

---
+++ src/components/LinkHelpIcon.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {capheight, scale} from 'src/utils/designSpecs';
+import {color} from 'src/utils/color';
+import {mediaQuery} from 'src/utils/breakpoints';
+import remify from 'src/utils/remify';
+import {typescale} from 'src/utils/typography';
+
+// color scheme
+const black = color.input.background;
+const white = color.input.border;
+
+const Svg = styled.svg`
+  fill: ${black};
+  stroke: ${white};
+  stroke-width: 1.5;
+  width: ${remify(25)};
+  @media only screen and ${mediaQuery.font} {
+    width: ${remify(25 * scale)};
+  }
+
+  &:focus,
+  &:hover {
+    fill: ${white};
+    stroke: ${black};
+  }
+  &:active {
+    fill: ${black};
+    stroke: ${white};
+  }
+`;
+
+const LinkHelpIcon = ({href, linkTitle}) => {
+  return (
+    <a href={href} rel="help" title={linkTitle}>
+      {/* Adapted from the help icon by Tabler Icon https://tablericons.com/ */}
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        transform="translate(0, 4)"
+      >
+        <title>{linkTitle}</title>
+        <circle cx="12" cy="12" r="9" />
+        <line x1="12" y1="16" x2="12" y2="18" />
+        <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
+      </Svg>
+    </a>
+  );
+};
+
+LinkHelpIcon.propTypes = {
+  href: PropTypes.string.isRequired,
+  linkTitle: PropTypes.string,
+};
+
+export default LinkHelpIcon;
---
+++ src/components/LinkHelpIcon.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {axe} from 'jest-axe';
+
+import LinkHelpIcon from './LinkHelpIcon';
+
+const mockProps = {
+  href: 'https://triangulum.netlify.app',
+  linkTitle: 'Link for learning what chroma is',
+};
+
+test('renders UI correctly', () => {
+  const {container} = render(<LinkHelpIcon {...mockProps} />);
+  expect(container).toMatchInlineSnapshot(`
+    .c0 {
+      fill: rgb(10,10,10);
+      stroke: rgb(255,255,255);
+      stroke-width: 1.5;
+      width: 1.5625rem;
+    }
+
+    .c0:focus,
+    .c0:hover {
+      fill: rgb(255,255,255);
+      stroke: rgb(10,10,10);
+    }
+
+    .c0:active {
+      fill: rgb(10,10,10);
+      stroke: rgb(255,255,255);
+    }
+
+    @media only screen and (min-width:45.5rem) {
+      .c0 {
+        width: 1.875rem;
+      }
+    }
+
+    <div>
+      <a
+        href="https://triangulum.netlify.app"
+        rel="help"
+        title="Link for learning what chroma is"
+      >
+        <svg
+          class="c0"
+          transform="translate(0, 4)"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>
+            Link for learning what chroma is
+          </title>
+          <circle
+            cx="12"
+            cy="12"
+            r="9"
+          />
+          <line
+            x1="12"
+            x2="12"
+            y1="16"
+            y2="18"
+          />
+          <path
+            d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4"
+          />
+        </svg>
+      </a>
+    </div>
+  `);
+});
+
+test('is accessible', async () => {
+  const {container} = render(<LinkHelpIcon {...mockProps} />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
