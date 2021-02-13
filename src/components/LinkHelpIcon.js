c14b1cb0f965610d39224f755986c7d5f48b007c
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-14 08:47:04 +0900

    fix(linkhelpicon): redesign the link help icon
    make the question mark more familiar in its shape by extending the stem

--- src/components/LinkHelpIcon.js
+++ src/components/LinkHelpIcon.js
@@ -50,6 +50,7 @@ const LinkHelpIcon = ({href, linkTitle}) => {
         <title data-testid="svg-title">{linkTitle}</title>
         <circle cx="12" cy="12" r="9" />
         <line x1="12" y1="16" x2="12" y2="18" />
+        <line x1="12" y1="13.5" x2="12" y2="14.5" />
         <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
       </Svg>
     </a>
