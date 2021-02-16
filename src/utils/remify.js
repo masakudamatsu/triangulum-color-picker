0c4c36afd493bcd1351a75e5a9a88312c2cd9a14
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-16 10:36:34 +0900

    test(remify.js): add tests for invalid arguments
    show the meaningful error message when the argument for remify() is invalid

--- src/utils/remify.js
+++ src/utils/remify.js
@@ -1,6 +1,16 @@
 import round from './round';
 
 function remify(px) {
+  if (px === undefined) {
+    throw new Error(
+      'The argument is missing. Provide a number of the pixel value to be converted into rem.',
+    );
+  }
+  if (typeof px !== 'number') {
+    throw new Error(
+      `The argument must be a number, but you provided a ${typeof px}`,
+    );
+  }
   const oneRemInPx = 16;
   function stringify(number) {
     return `${number.toString()}rem`;
