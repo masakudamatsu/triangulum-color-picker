ad7cdb0ead312b3478da3a116e48fc92f4bdd861
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-19 16:49:58 +0900

    fix(textcrop): revise text crop parameters
    provide the correct parameters for actual size rendering. The previous parameters are based on
    magnified font size for which Poppins behave unexpectedly

--- src/utils/specFont.js
+++ src/utils/specFont.js
@@ -25,8 +25,8 @@ export const typescale = {
     xHeightRatio: 2,
     betweenLinesRatio: 3,
     textCropEm: {
-      top: -0.3855,
-      bottom: -0.406,
+      top: -0.3165, // from -0.282 to -0.351
+      bottom: -0.4195, // from -0.385 to -0.454
     },
     fontMetrics: poppins.light,
   },
@@ -35,8 +35,8 @@ export const typescale = {
     xHeightRatio: 3,
     betweenLinesRatio: 4,
     textCropEm: {
-      top: -0.318,
-      bottom: -0.336,
+      top: -0.304, // from -0.281 to -0.327
+      bottom: -0.3325, // from -0.305 to -0.350
     },
     fontMetrics: poppins.light,
   },
@@ -45,8 +45,8 @@ export const typescale = {
     xHeightRatio: 4,
     betweenLinesRatio: 5,
     textCropEm: {
-      top: -0.288,
-      bottom: -0.301,
+      top: -0.263, // -0.246 to -0.280
+      bottom: -0.316, // -0.299 to -0.333
     },
     fontMetrics: poppins.light,
   },
@@ -55,8 +55,8 @@ export const typescale = {
     xHeightRatio: 5,
     betweenLinesRatio: 6,
     textCropEm: {
-      top: -0.249,
-      bottom: -0.255,
+      top: -0.24, // -0.235 to - 0.245
+      bottom: -0.2575, // -0.252 to -0.263
     },
     fontMetrics: poppins.thin,
   },
@@ -65,8 +65,8 @@ export const typescale = {
     xHeightRatio: 4,
     betweenLinesRatio: 5,
     textCropEm: {
-      top: -0.281,
-      bottom: -0.295,
+      top: -0.264, // -0.247 to -0.281
+      bottom: -0.281, // -0.264 to -0.298
     },
     fontMetrics: poppins.thin,
   },
