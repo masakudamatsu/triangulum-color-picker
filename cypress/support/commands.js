e02ef6d9d4781058d39a920609bc9a43099b7d04
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-19 12:51:22 +0900

    test(cypress-image-snapshot): make the test failure threshold tougher
    detect tiny layout changes in image snapshot tests
    
    close #158

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,8 +12,8 @@ if (Cypress.config('isInteractive')) {
     capture: 'viewport', // this option is ignored for DOM-element snapshots
 
     // Allow tiny differences between snapshots
-    customDiffConfig: {threshold: 0.1}, // threshold for each pixel
-    failureThreshold: 0.009, // threshold for entire image
+    customDiffConfig: {threshold: 0.1}, // threshold for each pixel (0.1 is the recommended value by Cypress: https://github.com/jaredpalmer/cypress-image-snapshot#options)
+    failureThreshold: 0.0005, // threshold for entire image (the recommended 0.03 is way too generous)
     failureThresholdType: 'percent', // percent of image or number of pixels
 
     // see https://github.com/jaredpalmer/cypress-image-snapshot#options
